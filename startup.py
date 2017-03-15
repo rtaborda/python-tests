48e49092eb1163505f6c97a69b031659fc7be0a9
Rui Taborda <RTaborda@intuition.com>
2017-03-15 12:35:01 +0000

    Fix regarding variable names; Added .gitignore file

--- startup.py
+++ startup.py
@@ -1,24 +1,24 @@
 from subprocess import Popen
 
 class Startup:
-	def __init__(self, programs):
-		self.programs = programs
+	def __init__(self, paths):
+		self.paths = paths
 
 	def Start(self):
-		for path in programs:
+		for path in self.paths:
 			try:
 				Popen([path])
 			except:
 				print("There was an error openning the program with path: " + path)
 				pass
 
-programs = [
+programsPaths = [
 	r"C:\Program Files (x86)\Skype\Phone\Skype.exe",					# Skype
 	r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe",		# Chrome
 	r"C:\Users\RTaborda\AppData\Roaming\Spotify\Spotify.exe",			# Spotify
 	r"C:\Program Files (x86)\Notepad++\notepad++.exe",					# Notepad++
 	r"C:\Program Files (x86)\Microsoft Office\Office16\OUTLOOK.EXE"		# Outlook
 ]
-s = Startup(programs)
+s = Startup(programsPaths)
 s.Start()
 
