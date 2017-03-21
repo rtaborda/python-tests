bc2b9761f6bf4c880d8b0b18079e597a52592fc4
Rui Taborda <RTaborda@intuition.com>
2017-03-21 14:17:03 +0000

    Separated binary search tree implementation from tests

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -66,30 +66,4 @@ class BinarySearchTree:
 		else:
 			# otherwise go to the node's right branch
 			self.__searchMaxValueSmallerThan(limit, node.right, ret)
-			
-			
 
-# Testing
-# TODO Look into unit testing in Python
-tree = BinarySearchTree()
-tree.add(11)
-tree.add(3)
-tree.add(54)
-tree.add(6)
-tree.add(42)
-tree.add(95)
-tree.add(2)
-tree.add(45)
-tree.add(24)
-tree.add(23)
-tree.add(34)
-
-
-print("Expected: 3 Result: " + str(tree.searchMaxValueSmallerThan(6)))
-print("Expected: 34 Result: " + str(tree.searchMaxValueSmallerThan(35)))
-print("Expected: 42 Result: " + str(tree.searchMaxValueSmallerThan(44)))
-print("Expected: 45 Result: " + str(tree.searchMaxValueSmallerThan(54)))
-print("Expected: 54 Result: " + str(tree.searchMaxValueSmallerThan(55)))
-print("Expected: 95 Result: " + str(tree.searchMaxValueSmallerThan(100)))
-	
-		
